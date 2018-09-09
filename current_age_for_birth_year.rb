671f82c19cba0745a8b8ddb5a36e596cf2be5dd1
Lyn <jen.l@mailfence.com>
2018-09-09 19:41:52 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,4 @@
-describe "current_age_for_birth_year method" do
+def current_age_for_birth_year method (1984)
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
  
