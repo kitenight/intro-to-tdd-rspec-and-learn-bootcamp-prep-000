8b249728a8eab79207e9289e0b91a5643fff35cd
Lyn <jen.l@mailfence.com>
2018-09-09 19:48:08 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,8 @@
 it "should return the current year for a person born in year 0" do
   age = current_age_for_birth_year(0)
+   expect(age).to eq(2003)
+end
+
 # age_of_person = current_age_for_birth_year(1984)
 
 # expect(age_of_person).to eq(19)
