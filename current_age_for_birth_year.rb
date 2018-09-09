c8e3f19a407b641daa9f3324846de9ce9b278388
Lyn <jen.l@mailfence.com>
2018-09-09 19:45:47 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,4 @@
+age_of_person = current_age_for_birth_year(1984)
 
-    age_of_person = current_age_for_birth_year(1984)
- 
-    expect(age_of_person).to eq(19)
-  end
-end
+expect(age_of_person).to eq(19)
+  
